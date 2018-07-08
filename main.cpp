39080c7433e8fadb36821dde5f235205ad065d00
PurplePachyderm <a.delval87@gmail.com>
2018-07-08 02:03:47 +0200

    Add cube display for demo purpose

--- main.cpp
+++ main.cpp
@@ -26,37 +26,67 @@ int main() {
 
 	/************************************************************************************************/
 
-		// set up vertex data (and buffer(s)) and configure vertex attributes
+			// set up vertex data (and buffer(s)) and configure vertex attributes
 
+		//Cube vertives
 		float vertices[] = {
-	        // positions          // colors
-	         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,
-	         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,
-	        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,
-	        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f
+	        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+	         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+	         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+	        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+	        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+	        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+	         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+	        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+	        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	    };
-	    unsigned int indices[] = {
-	        0, 1, 3, // first triangle
-	        1, 2, 3  // second triangle
-	    };
-	    unsigned int VBO, VAO, EBO;
+
+		unsigned int VBO, VAO;
 	    glGenVertexArrays(1, &VAO);
 	    glGenBuffers(1, &VBO);
-	    glGenBuffers(1, &EBO);
 
 	    glBindVertexArray(VAO);
 
 	    glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
 	    // position attribute
-	    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	    glEnableVertexAttribArray(0);
-	    // color attribute
-	    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	    // texture coord attribute
+	    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	    glEnableVertexAttribArray(1);
 
 
@@ -70,13 +100,15 @@ int main() {
     glm::mat4 viewMatrix = glm::mat4(1.0f);;
 	glm::mat4 projectionMatrix = glm::mat4(1.0f);;
 
-	modelMatrix = glm::rotate(modelMatrix, glm::radians(-10.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-	viewMatrix  = glm::translate(viewMatrix, glm::vec3(0.0f, 0.0f, -3.0f));
+	modelMatrix = glm::rotate(modelMatrix, glm::radians(40.0f), glm::vec3(1.0f, 0.5f, 0.0f));	//Model coords
+	viewMatrix  = glm::translate(viewMatrix, glm::vec3(0.0f, 0.0f, -2.0f));		//Space coords
 	projectionMatrix = glm::perspective(glm::radians(45.0f), (float)window.getWidth() / (float)window.getHeight(), 0.1f, 100.0f);
 
 	shader1.setModelMatrix(modelMatrix);
 	shader1.setViewMatrix(viewMatrix);
 	shader1.setProjectionMatrix(projectionMatrix);
+
+	glEnable(GL_DEPTH_TEST);
 
 
 	/************************************************************************************************/
@@ -90,11 +122,14 @@ int main() {
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		//Clean depth buffer
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 		// render the triangle
 		shader1.use();
 
 		glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		shader1.disable();
 
